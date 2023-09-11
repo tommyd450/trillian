9077a21d16bb1025e943809215ac2916cf5146ef
tommyd450 <tommyd811@gmail.com>
2023-09-11 10:10:28 +0100

    Updated sync script and Added Database Dockerfile

--- redhat/release/update-to-head.sh
+++ redhat/release/update-to-head.sh
@@ -56,6 +56,12 @@ fi
 git fetch origin $midstream_ref
 git checkout origin/$midstream_ref $custom_files
 
+# RHTAP writes its pipeline files to the root of ${redhat_ref}
+# Fetch those from origin and apply them to the the release branch
+# since we just wiped out our local copy with the upstream ref.
+git fetch origin $redhat_ref
+git checkout origin/$redhat_ref .tekton
+
 # Apply midstream patches
 if [[ -d redhat/patches ]] && [ "$(ls -A redhat/patches)" ]; then
   git apply redhat/patches/*
